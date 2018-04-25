7bde08f2a08d2b518055c189df15d9ae6dc3428f
unknown <grem_snoort@protonmail.com>
2018-04-25 13:26:43 +0900

    flying plane

--- core/src/com/mygdx/game/states/PlayState.java
+++ core/src/com/mygdx/game/states/PlayState.java
@@ -3,42 +3,45 @@ package com.mygdx.game.states;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.game.FlappyDemo;
+import com.mygdx.game.sprites.Plane;
 
-public class MenuState extends State{
+public class PlayState extends State {
 
-    private Texture background;
-    private Texture playButton;
+    //private Texture plane;
+    private Plane plane;
 
-    public MenuState(GameStateManager gsm) {
+    public PlayState(GameStateManager gsm) {
         super(gsm);
-        background = new Texture("background.jpg");
-        playButton = new Texture("button.png");
+        plane = new Plane(50, 300);
+        //plane = new Texture("plane.png");
+        camera.setToOrtho(false, 360, 640);
     }
 
     @Override
     protected void handleInput() {
         if(Gdx.input.justTouched())
         {
-            gsm.set(new PlayState(gsm));
+            plane.jump();
         }
     }
 
     @Override
     public void update(float dt) {
         handleInput();
+        plane.update(dt);
     }
 
     @Override
     public void render(SpriteBatch sb) {
+        sb.setProjectionMatrix(camera.combined);
         sb.begin();
-        sb.draw(background, 0, 0);
-        sb.draw(playButton, 200, 400, 300, 300);
+        sb.draw(plane.getPlane(), plane.getPosition().x, plane.getPosition().y);
         sb.end();
     }
 
     @Override
     public void dispose() {
-        background.dispose();
-        playButton.dispose();
+
     }
 }
