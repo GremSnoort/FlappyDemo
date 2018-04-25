6e138e48b967ea991157ede903a3e06c85a2fc4c
unknown <grem_snoort@protonmail.com>
2018-04-25 12:36:34 +0900

    first screen

--- core/src/com/mygdx/game/FlappyDemo.java
+++ core/src/com/mygdx/game/FlappyDemo.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.mygdx.game.states.GameStateManager;
+import com.mygdx.game.states.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
 	//public static final int WIDTH = 480;
@@ -22,6 +23,7 @@ public class FlappyDemo extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
 		Gdx.gl.glClearColor(1, 0, 0, 1);
+		gsm.push(new MenuState(gsm));
 		//img = new Texture("badlogic.jpg");
 	}
 
--- core/src/com/mygdx/game/states/MenuState.java
+++ core/src/com/mygdx/game/states/MenuState.java
@@ -1,10 +1,17 @@
 package com.mygdx.game.states;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MenuState extends State{
+
+    private Texture background;
+    private Texture playButton;
+
     public MenuState(GameStateManager gsm) {
         super(gsm);
+        background = new Texture("background.jpg");
+        playButton = new Texture("button.png");
     }
 
     @Override
@@ -19,11 +26,15 @@ public class MenuState extends State{
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.begin();
+        sb.draw(background, 0, 0);
+        sb.draw(playButton, 200, 400, 300, 300);
+        sb.end();
     }
 
     @Override
     public void dispose() {
-
+        background.dispose();
+        playButton.dispose();
     }
 }
